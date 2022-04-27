10b73aa282c2d1ca638324023f2e66d8f0b5e8bf
Nenkai <neenkaai@gmail.com>
2022-04-27 16:34:31 +0200

    VS Code Extension

---
+++ vscode-extension/README.md
@@ -0,0 +1,9 @@
+# Visual Studio Code Extension for Adhoc
+This is a lazily edited extension from the original Javascript extension adapted for adhoc.
+Provides syntax highlighting and snippets. (Recommended to browse with the [Snippet Viewer](https://marketplace.visualstudio.com/items?itemName=RandomFractalsInc.snippets-viewer).)
+
+## Building & Installing
+* `npm install -g vsce`
+* `vsce publish`
+
+Double click on the built `.vsix` file.
---
+++ vscode-extension/src/extension.ts
@@ -0,0 +1,25 @@
+'use strict';
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    var type = "build_script";
+    vscode.tasks.registerTaskProvider(type, {
+        provideTasks(token?: vscode.CancellationToken) {
+            if (vscode.workspace.workspaceFolders === undefined)
+                return;
+
+            var currentlyOpenTabfilePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+            if (currentlyOpenTabfilePath == undefined)
+                return undefined;
+
+            var execution = new vscode.ShellExecution(`GTAdhocToolchain build -i ${currentlyOpenTabfilePath}`);
+            return [
+                new vscode.Task({type: type}, vscode.TaskScope.Workspace,
+                    "Build Adhoc Script", "Adhoc", execution, undefined)
+            ];
+        },
+        resolveTask(task: vscode.Task, token?: vscode.CancellationToken) {
+            return task;
+        }
+    });
+}
