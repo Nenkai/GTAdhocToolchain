7d1e007b0238b1d223230f982c36493dd5467cbe
Nenkai <Nenkai@users.noreply.github.com>
2023-06-07 23:14:34 +0200

    Add `EVENTRESULT` built-in defines

--- GTAdhocToolchain.Preprocessor/BuiltinDefines.cs
+++ GTAdhocToolchain.Preprocessor/BuiltinDefines.cs
@@ -82,13 +82,17 @@ namespace GTAdhocToolchain.Preprocessor
             { "COLOR_CYAN", $"{0x16}" },
             { "COLOR_BLACK", $"{0x17}" },
 
-            // Event Result
+            // Event Result (old, should not be used)
             { "STATE_EXIT", $"{(int)AdhocRunState.EXIT}" },
             { "STATE_RETURN", $"{(int)AdhocRunState.RETURN}" },
             { "STATE_YIELD", $"{(int)AdhocRunState.YIELD}" },
             { "STATE_EXCEPTION", $"{(int)AdhocRunState.EXCEPTION}" },
             { "STATE_CALL", $"{(int)AdhocRunState.CALL}" },
             { "STATE_RUN", $"{(int)AdhocRunState.RUN}" },
+
+            { "EVENTRESULT_CONTINUE", $"{0}" },
+            { "EVENTRESULT_STOP", $"{1}" },
+            { "EVENTRESULT_FILTER", $"{2}" },
 
             // mScrollClip - scroll_mode
             { "SCROLL_MODE_FOLLOW_FOCUS", $"{0}" }, // follow_focus
