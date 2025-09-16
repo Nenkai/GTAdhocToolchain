9a257f7377b01682f416f38de09b88a0ce067239
Nenkai <Nenkai@users.noreply.github.com>
2025-09-16 19:30:11 +0200

    remove redundant old vs extension folder

--- vs-extension/Classification/ClassificationFormat.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.Composition;
-using System.Windows.Media;
-using Microsoft.VisualStudio.Text.Classification;
-using Microsoft.VisualStudio.Utilities;
-
-namespace AdhocLanguage
-{
-
-}
